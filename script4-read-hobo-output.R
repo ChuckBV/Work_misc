07d7f27c66e16a8452ef33c00208e04d01b9c635
Chuck Burks <r.dominica@gmail.com>
2023-02-06 19:39:13 -0800

    Update hobo script to read and plot Hobo data

--- script4-read-hobo-output.R
+++ script4-read-hobo-output.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(lubridate)
 library(readxl)
 library(janitor)
+library(readxl)
 
 theme_csb_fullwidth1 <- function(){
   theme_bw() +
@@ -21,6 +22,8 @@ theme_csb_fullwidth1 <- function(){
         legend.text = element_text(color = "black", size = 10)
   )
 }
+
+#-- 1. Read Feb 2023 csv file for new environmental controller --------------
 
 # Load data frame to Global Environment and clean
 
@@ -56,3 +59,39 @@ ggsave(filename = "hobo_plot_2023_02_06.jpg",
        dpi = 300, width = 5.83, height = 5.83, units = "in")
 
 
+#-- 2. Read a 2022 file for old environmental controller --------------
+
+old <- read_excel("h123-q2-2022-02-11-07-18-05-0800.xlsx",
+           sheet = "DATA",
+           skip = 1)
+
+old <- clean_names(old)
+
+
+old <- old %>% 
+  rename(date_time = date_time_gmt_0700,
+         deg_c = temp_c,
+         rh = rh_percent)
+
+# Select 3 days
+
+old <- old %>%  # narrow to October
+  filter(month(date_time) > 9 & month(date_time) < 11)
+
+old <- old %>% # narrow to first three days of October
+  filter(mday(date_time) < 4)
+
+p2 <- ggplot(old, aes(x = date_time, y = deg_c)) +
+  geom_line() +
+  labs(title = "Temperature logger readings, 5 min intervals",
+       x = "",
+       y = "Degree Celcius",
+       caption = "Queue2 in H123") +
+  ylim(12.5,34) +
+  theme_csb_fullwidth1()
+
+p2
+
+ggsave(filename = "hobo_plot_2022_10_03.jpg", 
+       plot = p2, device = "jpg", 
+       dpi = 300, width = 5.83, height = 5.83, units = "in")
